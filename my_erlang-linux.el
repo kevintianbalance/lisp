24f2ed4296b266300fd4a2b3d30c9e2b823ec97a
echetia <kevin.tian@ericsson.com>
2017-06-07 10:29:25 +0200

    [task] erlang mode work on linux

--- my_erlang-linux.el
+++ my_erlang-linux.el
@@ -2,8 +2,8 @@
 ;; erlang
 ;; D   README                1888  Org								c:/KevinTian/Program/erl5.9.1/lib/tools-2.6.7/emacs/README
 
-(setq load-path (cons  "~/bin/erl5.10.1/lib/tools-2.6.10/emacs/"
+(setq load-path (cons  "/home/echetia/KevinTian/bin/otp_bin_19.3/lib/erlang/lib/tools-2.9.1/emacs/"
 											 load-path))
-(setq erlang-root-dir "~/bin/erl5.10.1")
-(setq exec-path (cons "~/bin/erl5.10.1/bin" exec-path))
+(setq erlang-root-dir "/home/echetia/KevinTian/bin/otp_bin_19.3/lib/erlang")
+(setq exec-path (cons "/home/echetia/KevinTian/bin/otp_bin_19.3/lib/erlang/bin" exec-path))
 (require 'erlang-start)
